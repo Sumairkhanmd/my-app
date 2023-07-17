c1d7f6c9bcb56bdf84869c8f62ae787aa3847961
Sumairkhanmd <mailstosumair@gmail.com>
2023-07-17 20:12:56 +0530

    Changes to be committed: 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 
 function App() {
   return (
+    <>
+    <h1>this is me</h1>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,6 +21,7 @@ function App() {
         </a>
       </header>
     </div>
+    </>
   );
 }
 
