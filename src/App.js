9e108e3be241ce273745e942778b8f310cb6347a
Sumairkhanmd <mailstosumair@gmail.com>
2023-07-24 12:40:29 +0530

    modified:   src/App.js 	new file:   src/components/TextForm.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 
 function App() {
   return (
     <>
   <Navbar title="Nav bar" about="about me"/>
+  <div className="container my-3">
+  <TextForm heading="Enter your text here" title="Text Box"/>
+  </div>
   
     </>
   );
---
+++ src/components/TextForm.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+export default function TextForm(props) {
+    const [text, setText] = useState("Enter the text here2");
+    const handleUpClick=()=>{
+        console.log("clicked");
+        setText=("You have clicked on handleUpClick");
+    }
+    const handleOnChange=()=>{
+        console.log("changed");
+    }
+  return (
+    <>
+      <div>
+        <h1>{props.heading}</h1>
+        <form>
+          <div className="mb-3" />
+          <label htmlFor="My box" className="form-label">
+            {props.title}
+          </label>
+          <textarea
+            className="form-control"
+            id="My box"
+            rows="8"
+            value={text}
+            onChange={handleOnChange}
+          />
+        </form>
+      </div>
+      <br></br>
+      <button className="btn btn-primary" onClick={handleUpClick}>Convert to Upper Text</button>
+    </>
+  );
+}
