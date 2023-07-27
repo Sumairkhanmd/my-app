d454db71a5ae41f3d72f26dfc1efe63fefc08f88
Sumairkhanmd <mailstosumair@gmail.com>
2023-07-27 12:24:52 +0530

    modified:   src/components/Navbar.js 	modified:   src/components/TextForm.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 export default function Navbar(props) {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <a className="navbar-brand" href="/">
           {props.title}
         </a>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,18 +1,29 @@
 import React, { useState } from "react";
 
 export default function TextForm(props) {
-    const [text, setText] = useState("Enter the text here2");
+    const [text, setText] = useState("");
     const handleUpClick=()=>{
-        console.log("clicked");
-        setText("You have clicked on handleUpClick");
+        //console.log("clicked");
+        let newText=text.toUpperCase();
+        setText(newText);
     }
+    const handleclear=()=>{
+      //console.log("clicked");
+      let newText="";
+      setText(newText);
+  }
+    const handleLowClick=()=>{
+      //console.log("clicked");
+      let newText=text.toLowerCase();
+      setText(newText);
+  }
     const handleOnChange=(event)=>{
-        console.log("changed");
+        //console.log("changed");
         setText(event.target.value)
     }
   return (
     <>
-      <div>
+      <div className="container">
         <h1>{props.heading}</h1>
         <form>
           <div className="mb-3" />
@@ -29,7 +40,16 @@ export default function TextForm(props) {
         </form>
       </div>
       <br></br>
-      <button className="btn btn-primary" onClick={handleUpClick}>Convert to Upper Text</button>
+      <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Upper Text</button>
+      <button className="btn btn-primary mx-2" onClick={handleLowClick}>Convert to Lower Text</button>
+      <button className="btn btn-primary mx-2" onClick={handleclear}>Clear text</button>      
+      <div className="container my-3">
+        <h3>Text Summary</h3>
+        <p>The number of characters is: {text.length} and the number of words is: {text.split(" ").length-1}</p>
+        <p>The time required to read the above text is: {0.008*text.split(" ").length}</p>
+        <h3>Preview</h3>
+        <p>{text}</p>
+      </div>
     </>
   );
 }
